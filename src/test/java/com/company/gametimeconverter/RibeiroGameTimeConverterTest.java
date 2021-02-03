7813d5de768aea8f07a09a0270a65fe48ae57994
João Gonçalo Peixe Ribeiro <jgpribeiro@hotmail.com>
2021-02-03 17:20:50 +0000

    made test class public

--- src/test/java/com/company/gametimeconverter/RibeiroGameTimeConverterTest.java
+++ src/test/java/com/company/gametimeconverter/RibeiroGameTimeConverterTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-class RibeiroGameTimeConverterTest {
+public class RibeiroGameTimeConverterTest {
 
     private RibeiroGameTimeConverter dateConverter;
 
